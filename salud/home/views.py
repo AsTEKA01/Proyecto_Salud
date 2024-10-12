ac94a202c635f4771ed9dba8dad14e4aa00bffab
EstebanR054 <esteban.roa7@soy.sena.edu.co>
2024-10-12 01:10:25 -0500

    Update table, and models

--- salud/home/views.py
+++ salud/home/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 from .obervaciones_lab import OrdenLaboratorios
 # views.py
+
 def name_sal(request):
     if request.user.is_authenticated:
         context = {'user': request.user}
-        return render(request, 'home.html', context)
-    
-def name_sal(request):
-    observaciones = OrdenLaboratorios.objects.all()
-    return render(request, 'home.html', {'observaciones': observaciones}) 
+        
+@login_required
+def list_orden(request):
+    usuario = request.user
+    ordenes = OrdenLaboratorios.objects.filter(paciente=usuario)
+
+    # Obtener filtros del formulario
+    numero_orden = request.GET.get('numero_orden')
+    fecha_inicio = request.GET.get('fecha_inicio')
+    fecha_fin = request.GET.get('fecha_fin')
+
+    # Aplicar filtros si están presentes
+    if numero_orden:
+        ordenes = ordenes.filter(numero_orden__icontains=numero_orden)
+
+    if fecha_inicio and fecha_fin:
+        ordenes = ordenes.filter(fecha_orden__range=[fecha_inicio, fecha_fin])
+        
+    # Paginación
+    paginator = Paginator(ordenes, 5)  # 5 órdenes por página
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'home.html', {'ordenes': ordenes})
--- salud/salud/urls.py
+++ salud/salud/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from login.views import custom_login_view
-from home.views import name_sal
+from home.views import list_orden
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', custom_login_view, name='login'),
-    path('home/', name_sal, name='home'),
+    path('home/', list_orden, name='home'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
