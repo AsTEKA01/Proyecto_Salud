2597537a077667a928a57416969b2147e7c35720
EstebanR054 <esteban.roa7@soy.sena.edu.co>
2024-10-14 18:31:53 -0500

    Se agrega la landing page y imagenes nuevas

--- salud/salud/urls.py
+++ salud/salud/urls.py
@@ -7,13 +7,13 @@ from user_config.views import user_date
 from inf_pdf.views import generar_informe_pdf
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('login', custom_login_view, name='login'),
     path('home/', list_orden, name='home',),
     path('home/<int:id>/', ver_examen, name='ver_examen'),  # Verifica que el nombre sea 'ver_examen'
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('config', user_date, name='config'),
     path('home//<int:orden_id>/', generar_informe_pdf, name='generar_informe_pdf'),
-    path('landing/', landing_page_view, name="landing")
+    path('', landing_page_view, name="landing")
     
 ]
